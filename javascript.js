e9f1974142afd13cb2bf99d7860e91b55e45a77a
Rodrigo Goro <rodrigopamo323@gmail.com>
2023-02-13 18:57:24 -0500

    avanznado

--- javascript.js
+++ javascript.js
@@ -19,3 +19,31 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/*TIMER*/
+const $days = document.getElementById("days"),
+  $hours = document.getElementById("hours"),
+  $minutes = document.getElementById("minutes"),
+  $seconds = document.getElementById("seconds");
+/*Fecha del proximo partido*/
+const countdownDate = new Date("Feb 19, 2023 12:00:00").getTime();
+
+let interval = setInterval(function () {
+  //Fecha actual en milisegundos
+  const ahora = new Date().getTime();
+
+  //Obetner las distancias entre ambas fechas
+  let distance = countdownDate - ahora;
+
+  //Calculos a dias- horas -min -seg
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //Escribimos resultados
+  $days.innerHTML = "0" + days;
+  $hours.innerHTML = hours;
+  $minutes.innerHTML = minutes;
+  $seconds.innerHTML = ("0" + seconds).slice(-2);
+}, 1000);
